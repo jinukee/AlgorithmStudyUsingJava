089e544e48eeff0bbdaf0c67726bb0e7b9fdd13f
jinukee <87477498+jinukee@users.noreply.github.com>
2025-09-20 01:14:47 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
