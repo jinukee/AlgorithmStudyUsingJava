a0a67eafdae35aef4b308c943032bf1e4f4415e5
jinukee <87477498+jinukee@users.noreply.github.com>
2025-09-23 23:04:47 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16304 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 9; i++) {
+            bw.write(N + " * " + i + " = " + (i * N));
+            bw.write("\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
