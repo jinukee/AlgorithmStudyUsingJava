5fb901328304b193821f5f0650e5e71070c37432
jinukee <87477498+jinukee@users.noreply.github.com>
2025-09-28 13:43:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char c = br.readLine().charAt(0);
+
+        bw.write((int) c + "");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
