f7c488672b7e87de1e6e9a94464f5c76405624ab
jinukee <87477498+jinukee@users.noreply.github.com>
2025-10-08 09:30:02 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.write(String.valueOf(Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken())));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
