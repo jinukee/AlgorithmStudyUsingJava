e752a6bb8eaad7614fff1fbba968f773fef6c4db
jinukee <87477498+jinukee@users.noreply.github.com>
2026-01-26 09:14:44 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final StringBuilder sb = new StringBuilder();
+    private static int[] numbers;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = i + 1;
+        }
+
+        combination(new int[M], 0, 0, M);
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void combination(int[] out, int start, int depth, int target) {
+        if (depth == target) {
+            for (int num : out) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < numbers.length; i++) {
+            out[depth] = numbers[i];
+            combination(out, i + 1, depth + 1, target);
+        }
+    }
+}
