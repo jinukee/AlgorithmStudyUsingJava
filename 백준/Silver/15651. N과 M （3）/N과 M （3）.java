1aad69255004eb00dc0efd64a46d2021cb45887f
jinukee <87477498+jinukee@users.noreply.github.com>
2026-01-26 12:39:30 +0900

    [Silver III] Title: N과 M (3), Time: 496 ms, Memory: 69252 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[] numbers;
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = i + 1;
+        }
+
+        permutation(new int[M], 0, M);
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void permutation(int[] out, int depth, int target) {
+        if (depth == target) {
+            for (int num : out) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int number : numbers) {
+            out[depth] = number;
+            permutation(out, depth + 1, target);
+        }
+    }
+}
