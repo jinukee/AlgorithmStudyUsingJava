b36b5197d1c09158dbe4031b282c6aa46b19d717
jinukee <87477498+jinukee@users.noreply.github.com>
2026-02-10 13:34:31 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 840 ms, Memory: 90976 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static final StringBuilder sb = new StringBuilder();
+    private static final List<List<Integer>> adj = new ArrayList<>();
+    private static boolean[] visited;
+    private static int count = 1;
+    private static int[] result;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N + 1; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        for (List<Integer> neighbors : adj) {
+            Collections.sort(neighbors);
+        }
+
+        visited = new boolean[N + 1];
+        result = new int[N + 1];
+        bfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(result[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        visited[start] = true;
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            result[now] = count++;
+            for (int neighbor : adj.get(now)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.offer(neighbor);
+                }
+            }
+        }
+    }
+}
