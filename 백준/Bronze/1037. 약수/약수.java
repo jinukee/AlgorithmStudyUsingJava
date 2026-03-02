680bc3a09117faf051de17bbe34a66ca60e62e6a
jinukee <87477498+jinukee@users.noreply.github.com>
2026-03-02 15:58:51 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        System.out.println(nums[0] * nums[N - 1]);
+    }
+}
