22b6997c8568e4ac767aff4888035f2f566daf4f
jinukee <87477498+jinukee@users.noreply.github.com>
2026-03-06 17:45:42 +0900

    [Gold IV] Title: 연구소, Time: 388 ms, Memory: 71612 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class Main {
     private static int N, M;
-    private static int[][] initialGrid; // 초기 격자 저장
-    private static int[][] grid; // 벽이 세워진 격자 정보도 전역으로 관리
+    private static int[][] grid;
+    private static int[][] visited;
+    private static int visitId = 0; // 방문 처리를 위한 배열을 매번 초기화 하지 않고, 방문 id를 늘려가며 gc 부하를 줄이는 기법, 알아두면 유용할듯
+
+    private static final List<int[]> startPoints = new ArrayList<>();
+    private static final List<int[]> emptySpaces = new ArrayList<>();
+
     private static int safe = -3; // 3개의 안전 지역은 벽으로 대체될 것이기 때문에 미리 3빼기
     private static int ans = 0;
 
@@ -18,10 +23,9 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        initialGrid = new int[N][M];
 
-        List<int[]> startPoints = new ArrayList<>(); // 바이러스 전파 시작점 저장
-        List<int[]> changeable = new ArrayList<>(); // 벽이 세워질 수 있는 모든 지점(0) 저장
+        grid = new int[N][M];
+        visited = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -29,43 +33,35 @@ public class Main {
                 int now = Integer.parseInt(st.nextToken());
                 if (now == 0) {
                     safe++;
-                    changeable.add(new int[]{i, j});
+                    emptySpaces.add(new int[]{i, j});
                 } else if (now == 2) {
                     startPoints.add(new int[]{i, j});
                 }
-                initialGrid[i][j] = now;
+                grid[i][j] = now;
             }
         }
 
-        solution(changeable, startPoints);
+        selectWalls(0, 0);
         System.out.println(ans);
     }
 
-    private static void solution(List<int[]> changeable, List<int[]> startPoints) {
-        int len = changeable.size();
 
-        // 벽은 3개만 세우면 되므로 n개중 3개 고르는 combination을 그냥 반복문으로 구현
-        for (int i = 0; i < len - 2; i++) {
-            for (int j = i + 1; j < len - 1; j++) {
-                for (int k = j + 1; k < len; k++) {
-                    grid = new int[N][M];
-                    for (int x = 0; x < N; x++) {
-                        for (int y = 0; y < M; y++) {
-                            grid[x][y] = initialGrid[x][y];
-                        }
-                    }
+    private static void selectWalls(int start, int count) {
+        if (count == 3) { // 3개 세웠으면 bfs 탐색 진행 후 리턴
+            bfs();
+            return;
+        }
 
-                    grid[changeable.get(i)[0]][changeable.get(i)[1]] = 1;
-                    grid[changeable.get(j)[0]][changeable.get(j)[1]] = 1;
-                    grid[changeable.get(k)[0]][changeable.get(k)[1]] = 1;
-
-                    bfs(startPoints);
-                }
-            }
+        for (int i = start; i < emptySpaces.size(); i++) {
+            int[] pos = emptySpaces.get(i);
+            grid[pos[0]][pos[1]] = 1; // 벽 하나 세우기
+            selectWalls(i + 1, count + 1);
+            grid[pos[0]][pos[1]] = 0; // 세운 벽 되돌리기(backtrack)
         }
     }
 
-    private static void bfs(List<int[]> startPoints) {
+    private static void bfs() {
+        visitId++; // 방문배열 초기화 하지 말고 방문 id 증가시키는 기법을 사용하여 gc 부담 줄이기
         Queue<int[]> q = new ArrayDeque<>(startPoints);
         int count = 0;
         while (!q.isEmpty()) {
@@ -81,7 +77,7 @@ public class Main {
                         return;
                     }
                     // 감염처리 후 계속 전파를 위해 q에 삽입
-                    grid[nx][ny] = 2;
+                    visited[nx][ny] = visitId;
                     q.offer(new int[]{nx, ny});
                 }
             }
@@ -90,6 +86,6 @@ public class Main {
     }
 
     private static boolean canGo(int x, int y) {
-        return x >= 0 && y >= 0 && x < N && y < M && grid[x][y] == 0;
+        return x >= 0 && y >= 0 && x < N && y < M && grid[x][y] == 0 && visited[x][y] != visitId;
     }
 }
