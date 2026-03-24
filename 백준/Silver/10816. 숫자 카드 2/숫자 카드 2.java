d98a18b579383d2b264ec8274b710a07d89516b3
Lee jinwook <87477498+jinukee@users.noreply.github.com>
2026-03-24 09:37:43 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1392 ms, Memory: 121468 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -4,75 +4,55 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            sb.append(count(arr, Integer.parseInt(st.nextToken())));
-            sb.append(" ");
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(arr, target) - lowerBound(arr, target)).append(" ");
         }
+        System.out.print(sb);
 
-        System.out.println(sb);
     }
 
-    private static int count(int[] arr, int target) {
-        int first = findFirst(arr, target);
-        if (first == -1) {
-            return 0;
-        }
-        int last = findLast(arr, target);
-        return last - first + 1;
-    }
-
-    private static int findFirst(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        int result = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                result = mid;
-                right = mid - 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
+    private static int lowerBound(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] >= target) {
+                r = mid;
             } else {
-                right = mid - 1;
+                l = mid + 1;
             }
         }
-        return result;
+        return l;
     }
 
-    private static int findLast(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        int result = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                result = mid;
-                left = mid + 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
+    private static int upperBound(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] <= target) {
+                l = mid + 1;
             } else {
-                right = mid - 1;
+                r = mid;
             }
         }
-        return result;
+        return l;
     }
 }
