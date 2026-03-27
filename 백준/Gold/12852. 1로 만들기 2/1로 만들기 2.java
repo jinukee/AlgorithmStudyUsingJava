3140f3b19e7fa44616e259e7066338435116c6d3
Lee jinwook <87477498+jinukee@users.noreply.github.com>
2026-03-27 16:30:01 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 21984 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -9,45 +9,46 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[] dp = new int[N + 3];
+        int[] path = new int[N + 3];
 
         dp[2] = 1;
         dp[3] = 1;
-
+        path[2] = 1;
+        path[3] = 1;
         if (N <= 3) {
             System.out.println(dp[N]);
-            printResult(N, dp);
+            printResult(N, path);
             return;
         }
 
         for (int i = 4; i <= N; i++) {
             dp[i] = dp[i - 1] + 1;
+            int prev = i - 1;
             if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                if (dp[i] > dp[i / 2] + 1) {
+                    dp[i] = dp[i / 2] + 1;
+                    prev = i / 2;
+                }
             }
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+                if (dp[i] > dp[i / 3] + 1) {
+                    dp[i] = dp[i / 3] + 1;
+                    prev = i / 3;
+                }
             }
+            path[i] = prev;
         }
 
         System.out.println(dp[N]);
-        printResult(N, dp);
+        printResult(N, path);
     }
 
-    static void printResult(int N, int[] dp) {
+    static void printResult(int N, int[] path) {
         StringBuilder sb = new StringBuilder();
-
         while (N > 1) {
             sb.append(N).append(" ");
-            int next = N - 1;
-            if (N % 2 == 0 && dp[next] > dp[N / 2]) {
-                next = N / 2;
-            }
-            if (N % 3 == 0 && dp[next] > dp[N / 3]) {
-                next = N / 3;
-            }
-            N = next;
+            N = path[N];
         }
-
         sb.append(1);
         System.out.println(sb);
     }
