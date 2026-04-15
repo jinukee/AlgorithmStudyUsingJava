c7d6a5493143560461c30470a3b7eeb9cb148ac9
Lee jinwook <87477498+jinukee@users.noreply.github.com>
2026-04-15 14:11:15 +0900

    [Gold IV] Title: 오큰수, Time: 928 ms, Memory: 189620 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(solution(arr, N));
+	}
+
+	static String solution(int[] arr, int n) {
+		StringBuilder sb = new StringBuilder();
+
+		Deque<Integer> stack = new ArrayDeque<>();
+		Deque<Integer> ans = new ArrayDeque<>();
+
+		int p = n - 1;
+		while (p >= 0) {
+			while (!stack.isEmpty() && stack.peek() <= arr[p]) {
+				stack.pop();
+			}
+
+			if (stack.isEmpty()) {
+				ans.push(-1);
+			} else {
+				ans.push(stack.peek());
+			}
+
+			stack.push(arr[p]);
+			p--;
+		}
+
+		while (!ans.isEmpty()) {
+			sb.append(ans.pop()).append(" ");
+		}
+
+		return sb.toString();
+	}
+}
